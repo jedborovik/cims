239bab161c5e9d6ed44a8996b67dcda0ae163ebb
Jed Borovik <mr.borovik@gmail.com>
2013-02-21 20:45:41 -0500

    begin matlab command

--- lib/command.rb
+++ lib/command.rb
@@ -1,3 +1,5 @@
+require 'net/ssh'
+
 module CIMS
     module Command
 
@@ -7,6 +9,14 @@ module CIMS
 
 	def self.login
 	    exec("ssh jeb561@access.cims.nyu.edu")
+	end
+
+	def self.matlab
+	    Net::SSH.start('access.cims.nyu.edu', 'jeb561', password: "xxxxxxxxx") do |ssh|
+		Net::SSH.start('i5.nyu.edu', 'jeb561', password: "xxxxxxxxx") do |ssh2| 
+		    puts ssh2.exec!("ls")
+		end
+	    end
 	end
 	
     end
